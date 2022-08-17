4b313744cfe44301ab75ee544c250b017b163d39
Shashwata98 <sk.prottoy.sk@gmail.com>
2022-08-17 18:48:06 -0400

    adding a new project

---
+++ Selenium-Project/src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import browser.Browser;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class FirstTest {
+    @Test
+    public void test1() {
+        Browser.setDriver();
+    }
+}
